f281583baf04bfbbd05fcd84d38778b1a52c0ba9
RTTArmel <tsiorytah@gmail.com>
2019-07-22 08:36:33 +0300

    reinitialisation

--- Config/dbconfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: "mongodb+srv://tsiorytah:rta150694@tsiorytah-e4buz.azure.mongodb.net/test?retryWrites=true&w=majority",
-    //url: 'mongodb://localhost:27017/tsiory'
-}
