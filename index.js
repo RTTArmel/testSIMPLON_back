6afc926bd66c28992e2f7b8cd385577233533873
RTTArmel <47269788+RTTArmel@users.noreply.github.com>
2019-07-22 11:18:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,11 @@ mongoose.connect(dbConfig.url, {
 });
 
 r(app)
-app.listen(process.env.PORT || 8080);  
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }).listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);  
 /* http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
